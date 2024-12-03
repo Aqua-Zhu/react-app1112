01fe0821332b720fec742b22d07e689705828c1e
Aqua-Zhu <z891202z@gmail.com>
2024-12-03 09:46:19 +0800

    1203

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:process.env.NPDE_ENV === 'production' ?'/react-app1112/':'/',
   plugins: [react()],
 })
